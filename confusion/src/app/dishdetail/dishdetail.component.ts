3db3648ed20f68a4950376e7477ed871510a5779
Aravind Reddy <mogiliaravindreddy@gmail.com>
2020-05-06 10:58:22 +0530

    saving changes to server includes put method

--- confusion/src/app/dishdetail/dishdetail.component.ts
+++ confusion/src/app/dishdetail/dishdetail.component.ts
@@ -24,6 +24,7 @@ export class DishdetailComponent implements OnInit {
     @ViewChild('cform') commentFormDirective;
     comment: Comment;
     commentForm: FormGroup;
+    dishcopy: Dish;
 
     formErrors = {
       'author': '',
@@ -54,7 +55,7 @@ export class DishdetailComponent implements OnInit {
      .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params
      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-     .subscribe((dish) => {this.dish = dish; this.setPrevNext(dish.id); },
+     .subscribe((dish) => {this.dish = dish;this.dishcopy = dish; this.setPrevNext(dish.id); },
       errmess => this.errMess = <any>errmess );
   }
 
@@ -75,7 +76,13 @@ export class DishdetailComponent implements OnInit {
   onSubmit() {
     this.comment = this.commentForm.value;
     console.log(this.comment);
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy)
+     .subscribe(dish => {
+       this.dish = dish; this.dishcopy = dish;
+     },
+     ),
+     errmess => { this.dish = null; this.dishcopy = null;this.errMess = <any>errmess; }
     this.commentFormDirective.resetForm();
     this.commentForm.reset({
       name: '',
